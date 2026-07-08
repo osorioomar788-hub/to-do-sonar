6f6fc853f70f615478602d8ec11f5aa38ec48d31
Omar Osorio <osorioomar788@gmail.com>
2026-07-08 17:21:44 -0600

    Configure ESLint for Jest test files
    Added Jest configuration to ESLint for test files.

--- eslint.config.js
+++ eslint.config.js
@@ -7,4 +7,26 @@ module.exports = defineConfig([
   {
     ignores: ['dist/*'],
   },
+  {
+    files: [
+      '**/__tests__/**/*.[jt]s?(x)',
+      '**/*.test.[jt]s?(x)',
+      '**/*.spec.[jt]s?(x)',
+      'jest.config.js',
+      'jest.setup.js',
+    ],
+    languageOptions: {
+      globals: {
+        jest: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeAll: 'readonly',
+        beforeEach: 'readonly',
+        afterAll: 'readonly',
+        afterEach: 'readonly',
+      },
+    },
+  },
 ]);
